ff6e15b126d927b8fe6b87cc35faeeb412cda81b
kminoda <koji.m.minoda@gmail.com>
2022-08-18 16:09:04 +0900

    apply pre-commit
    Signed-off-by: kminoda <koji.m.minoda@gmail.com>

--- .github/workflows/update-sync-param-files.py
+++ .github/workflows/update-sync-param-files.py
@@ -1,6 +1,8 @@
-import glob
-import yaml
 import argparse
+import glob
+
+import yaml
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,7 +10,7 @@ def main():
     args = parser.parse_args()
 
     tier4_launch_path = "/tmp/autoware.universe/launch/"
-    files = glob.iglob(tier4_launch_path + '**/*.param.yaml', recursive=True)
+    files = glob.iglob(tier4_launch_path + "**/*.param.yaml", recursive=True)
 
     files_str = []
     for file in files:
@@ -17,29 +19,24 @@ def main():
 
     src2dst = []
     for file in files_str:
-        src_list = file.split('/')[3:]
-        dst_list = ['autoware_launch', 'config'] + file.split('/')[4:5] + file.split('/')[6:]
-        src = ''
+        src_list = file.split("/")[3:]
+        dst_list = ["autoware_launch", "config"] + file.split("/")[4:5] + file.split("/")[6:]
+        src = ""
         for el in src_list:
-            src += el + '/'
+            src += el + "/"
         src = src[:-1]
-        dst = ''
+        dst = ""
         for el in dst_list:
-            dst += el + '/'
+            dst += el + "/"
         dst = dst[:-1]
-        src2dst.append({
-            "source": src,
-            "dest": dst
-        })
+        src2dst.append({"source": src, "dest": dst})
 
-    sync_param_file = [{
-        "repository": "autowarefoundation/autoware.universe",
-        "files": src2dst
-    }]
+    sync_param_file = [{"repository": "autowarefoundation/autoware.universe", "files": src2dst}]
 
     # save sync-param-files.yaml
     with open(args.output, "w") as f:
         yaml.dump(sync_param_file, f, sort_keys=False)
+
 
 if __name__ == "__main__":
     main()
