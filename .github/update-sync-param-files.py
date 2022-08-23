d20bb59bfc333445a1a62d23abe550a64db0fdda
kminoda <koji.m.minoda@gmail.com>
2022-08-23 09:11:49 +0900

    isolate src2dst calculation as a function
    Signed-off-by: kminoda <koji.m.minoda@gmail.com>

--- .github/update-sync-param-files.py
+++ .github/update-sync-param-files.py
@@ -3,6 +3,12 @@ from pathlib import Path
 
 import yaml
 
+def src_name_to_dst_name(src: Path) -> Path:
+    module_launch_pkg_name = src.parents[-3].stem # e.g. "tier4_control_launch"
+    file_path_under_config = src.relative_to(Path("launch") / module_launch_pkg_name / "config")
+    dst = Path("autoware_launch/config") / module_launch_pkg_name / file_path_under_config
+
+    return dst
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,10 +21,7 @@ def main():
     autoware_launch_dir = Path(args.input_universe_dir) / "launch"
     for config_path in autoware_launch_dir.glob("**/*.param.yaml"):
         src = config_path.relative_to(args.input_universe_dir)
-
-        module_launch_pkg_name = src.parents[-3].stem # e.g. "tier4_control_launch"
-        file_path_under_config = src.relative_to(Path("launch") / module_launch_pkg_name / "config")
-        dst = Path("autoware_launch/config") / module_launch_pkg_name / file_path_under_config
+        dst = src_name_to_dst_name(src)
 
         sync_files_config.append({"source": str(src), "dest": str(dst)})
     sync_param_file = [
