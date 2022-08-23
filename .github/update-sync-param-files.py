8c076f6c0d0d6c4f8f68ea3a81dbb0ab3a71d085
kminoda <koji.m.minoda@gmail.com>
2022-08-23 09:33:16 +0900

    debugging
    Signed-off-by: kminoda <koji.m.minoda@gmail.com>

--- .github/update-sync-param-files.py
+++ .github/update-sync-param-files.py
@@ -6,6 +6,7 @@ import yaml
 
 def src_name_to_dst_name(src: Path) -> Path:
     print(src, len(src.parents))
+    print(src.parents[-1], src.parents[-2])
     module_launch_pkg_name = src.parents[-3].stem  # e.g. "tier4_control_launch"
     print(module_launch_pkg_name)
     file_path_under_config = src.relative_to(Path("launch") / module_launch_pkg_name / "config")
