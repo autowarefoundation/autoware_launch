3f28b1c6a26febace570cece5a6c9b0d2af2dcaa
veqcc <ryuta.kambe@tier4.jp>
2025-02-21 13:33:32 +0900

    Apply Agnocast
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- autoware_launch/launch/pointcloud_container.launch.py
+++ autoware_launch/launch/pointcloud_container.launch.py
@@ -12,8 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.actions import GroupAction
+from launch.actions import OpaqueFunction
+from launch.actions import SetEnvironmentVariable
 from launch.actions import SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
@@ -22,22 +27,7 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 
-def generate_launch_description():
-    def add_launch_arg(name: str, default_value=None):
-        return DeclareLaunchArgument(name, default_value=default_value)
-
-    set_container_executable = SetLaunchConfiguration(
-        "container_executable",
-        "component_container",
-        condition=UnlessCondition(LaunchConfiguration("use_multithread")),
-    )
-
-    set_container_mt_executable = SetLaunchConfiguration(
-        "container_executable",
-        "component_container_mt",
-        condition=IfCondition(LaunchConfiguration("use_multithread")),
-    )
-
+def launch_setup(context, *args, **kwargs):
     glog_component = ComposableNode(
         package="autoware_glog_component",
         plugin="autoware::glog_component::GlogComponent",
@@ -48,10 +38,36 @@ def generate_launch_description():
     pointcloud_container = ComposableNodeContainer(
         name=LaunchConfiguration("container_name"),
         namespace="/",
-        package="rclcpp_components",
+        package="agnocastlib",
         executable=LaunchConfiguration("container_executable"),
         composable_node_descriptions=[glog_component],
         output="both",
+    )
+
+    return [
+        GroupAction(actions=[
+            SetEnvironmentVariable(
+                name="LD_PRELOAD", value=f"libagnocast_heaphook.so:{os.getenv('LD_PRELOAD', '')}"),
+            SetEnvironmentVariable(name="MEMPOOL_SIZE", value="10737418240"),  # 10.24GB
+            pointcloud_container
+        ])
+    ]
+
+
+def generate_launch_description():
+    def add_launch_arg(name: str, default_value=None):
+        return DeclareLaunchArgument(name, default_value=default_value)
+
+    set_container_executable = SetLaunchConfiguration(
+        "container_executable",
+        "agnocast_component_container",
+        condition=UnlessCondition(LaunchConfiguration("use_multithread")),
+    )
+
+    set_container_mt_executable = SetLaunchConfiguration(
+        "container_executable",
+        "agnocast_component_container_mt",
+        condition=IfCondition(LaunchConfiguration("use_multithread")),
     )
 
     return LaunchDescription(
@@ -60,6 +76,6 @@ def generate_launch_description():
             add_launch_arg("container_name", "pointcloud_container"),
             set_container_executable,
             set_container_mt_executable,
-            pointcloud_container,
+            OpaqueFunction(function=launch_setup)
         ]
     )
