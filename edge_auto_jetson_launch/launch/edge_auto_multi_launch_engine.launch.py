e3719e631d259fcca8c35672a80dbe271193f338
YuxuanLiuTier4Desktop <619684051@qq.com>
2025-03-12 14:07:35 +0900

    first formulation of X2 v4.1.0
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>

---
+++ edge_auto_jetson_launch/launch/edge_auto_multi_launch_engine.launch.py
@@ -0,0 +1,195 @@
+# Copyright 2024 TIER IV, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import launch
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+import json
+from distutils.util import strtobool
+
+
+def create_camera_container(camera_id, container_name, use_multithread):
+    package = FindPackageShare("edge_auto_jetson_launch")
+    include = PathJoinSubstitution(
+        [package, f"launch/camera_common/camera_container.launch.xml"]
+    )
+    local_container_name = container_name + str(camera_id)
+    arguments = [
+        ("camera_id", str(camera_id)),
+        ("container_name", local_container_name),
+        ("use_multithread", use_multithread),
+    ]
+    return IncludeLaunchDescription(include, launch_arguments=arguments)
+
+
+def create_object_recognition(
+    camera_id, container_name, yolox_precision, process_index, build_engine_only
+):
+    package = FindPackageShare("edge_auto_jetson_launch")
+    include = PathJoinSubstitution(
+        [package, f"launch/object_recognition/object_recognition.launch.xml"]
+    )
+    container_name += str(camera_id)
+    arguments = [
+        ("camera_id", str(camera_id)),
+        ("roi_id", str(camera_id)),
+        ("container_name", container_name),
+        ("dla_core_id", str(-1)),  # 0 or 1, select -1 to run on GPU
+        ("yolox_precision", yolox_precision),
+        ("build_engine_only", build_engine_only),
+    ]
+    return IncludeLaunchDescription(include, launch_arguments=arguments)
+
+
+def create_traffic_light_recognition(camera_id, container_name, build_engine_only):
+    package = FindPackageShare("edge_auto_jetson_launch")
+    include = PathJoinSubstitution(
+        [
+            package,
+            f"launch/traffic_light_recognition/traffic_light_recognition.launch.xml",
+        ]
+    )
+    container_name += str(camera_id)
+    camera_name = "camera" + str(camera_id)
+    arguments = [
+        ("camera_name", camera_name),
+        ("container", container_name),
+        ("build_engine_only", build_engine_only),
+    ]
+    return IncludeLaunchDescription(include, launch_arguments=arguments)
+
+
+def create_camera_driver(camera_id, container_name):
+    package = FindPackageShare("edge_auto_jetson_launch")
+    include = PathJoinSubstitution(
+        [package, f"launch/camera_common/v4l2_camera.launch.xml"]
+    )
+    container_name += str(camera_id)
+    arguments = [("camera_id", str(camera_id)), ("container_name", container_name)]
+    return IncludeLaunchDescription(include, launch_arguments=arguments)
+
+
+def create_image_decompressor(camera_id, container_name):
+    package = FindPackageShare("edge_auto_jetson_launch")
+    include = PathJoinSubstitution(
+        [package, f"launch/camera_common/image_transport_decompressor.launch.xml"]
+    )
+    container_name += str(camera_id)
+    arguments = [("camera_id", str(camera_id)), ("container_name", container_name)]
+    return IncludeLaunchDescription(include, launch_arguments=arguments)
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # Load all camera ids
+    object_recognition_camera_ids = LaunchConfiguration(
+        "object_recognition_camera_ids"
+    ).perform(context)
+    traffic_light_camera_ids = LaunchConfiguration("traffic_light_camera_ids").perform(
+        context
+    )
+    camera_driver_camera_ids = LaunchConfiguration("camera_driver_camera_ids").perform(
+        context
+    )
+    live_sensor = LaunchConfiguration("live_sensor").perform(context)
+    container_name = LaunchConfiguration("container_name").perform(context)
+    yolox_precision = LaunchConfiguration("yolox_precision").perform(context)
+    use_multi_thread = LaunchConfiguration("use_multi_thread").perform(context)
+    build_engine_only = LaunchConfiguration("build_engine_only").perform(context)
+
+    # Convert string to list safely
+    object_recognition_camera_ids = json.loads(object_recognition_camera_ids)
+    camera_driver_camera_ids = json.loads(camera_driver_camera_ids)
+    traffic_light_camera_ids = json.loads(traffic_light_camera_ids)
+
+    # containers will be used for object recognition and traffic light recognition
+    # so we need to merge them into one set to avoid duplication
+    camera_containers = set(object_recognition_camera_ids + traffic_light_camera_ids)
+
+    camera_containers = [
+        create_camera_container(camera_id, container_name, use_multi_thread)
+        for camera_id in camera_containers
+    ]
+
+    # object recognition
+    object_recognitions = [
+        create_object_recognition(
+            camera_id, container_name, yolox_precision, process_index, build_engine_only
+        )
+        for process_index, camera_id in enumerate(object_recognition_camera_ids)
+    ]
+
+    traffic_light_recognitions = [
+        create_traffic_light_recognition(camera_id, container_name, build_engine_only)
+        for camera_id in traffic_light_camera_ids
+    ]
+    # cspell: ignore decompressors
+    # camera driver and image decompressor
+    if bool(strtobool(live_sensor)):
+        camera_drivers = [
+            create_camera_driver(camera_id, container_name)
+            for camera_id in camera_driver_camera_ids
+        ]
+        image_decompressors = []
+    if not bool(strtobool(live_sensor)):
+        camera_drivers = []
+        image_decompressors = [
+            create_image_decompressor(camera_id, container_name)
+            for camera_id in camera_driver_camera_ids
+        ]
+
+    return (
+        camera_containers
+        + object_recognitions
+        + traffic_light_recognitions
+        + camera_drivers
+        + image_decompressors
+    )
+
+
+def generate_launch_description():
+    return launch.LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "object_recognition_camera_ids",
+                description="camera index list for object recognition",
+            ),
+            DeclareLaunchArgument(
+                "traffic_light_camera_ids",
+                description="camera index list for traffic light recognition",
+            ),
+            DeclareLaunchArgument(
+                "camera_driver_camera_ids",
+                description="camera index list for starting camera driver",
+            ),
+            DeclareLaunchArgument(
+                "live_sensor", description="live camera driver or not"
+            ),
+            DeclareLaunchArgument(
+                "container_name", description="container name for object recognition"
+            ),
+            DeclareLaunchArgument(
+                "use_multithread", description="use multithread container or not"
+            ),
+            DeclareLaunchArgument(
+                "build_engine_only", description="build engine only or not"
+            ),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
---
+++ edge_auto_jetson_launch/launch/traffic_light_recognition/traffic_light_node_container.launch.py
@@ -0,0 +1,186 @@
+# Copyright 2020 Tier IV, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import SetLaunchConfiguration
+from launch.conditions import IfCondition
+from launch.conditions import UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from launch_ros.parameter_descriptions import ParameterFile
+
+
+def generate_launch_description():
+    launch_arguments = []
+
+    def add_launch_arg(name: str, default_value=None, description=None):
+        # a default_value of None is equivalent to not passing that kwarg at all
+        launch_arguments.append(
+            DeclareLaunchArgument(name, default_value=default_value, description=description)
+        )
+
+    add_launch_arg("enable_fine_detection", "True")
+    add_launch_arg("input/image", "/sensing/camera/traffic_light/image_raw")
+    add_launch_arg("output/rois", "/perception/traffic_light_recognition/rois")
+    add_launch_arg(
+        "output/traffic_signals", "/perception/traffic_light_recognition/traffic_signals"
+    )
+    add_launch_arg(
+        "output/car/traffic_signals", "/perception/traffic_light_recognition/car/traffic_signals"
+    )
+    add_launch_arg(
+        "output/pedestrian/traffic_signals",
+        "/perception/traffic_light_recognition/pedestrian/traffic_signals",
+    )
+
+    add_launch_arg("use_intra_process", "False")
+    add_launch_arg("use_multithread", "False")
+    add_launch_arg("container", "traffic_light_node_container")
+
+    add_launch_arg("traffic_light_fine_detector_param_path")
+    add_launch_arg("car_traffic_light_classifier_param_path")
+    add_launch_arg("pedestrian_traffic_light_classifier_param_path")
+
+    def create_parameter_dict(*args):
+        result = {}
+        for x in args:
+            result[x] = LaunchConfiguration(x)
+        return result
+
+    traffic_light_nodes = [
+        ComposableNode(
+            package="autoware_traffic_light_classifier",
+            plugin="autoware::traffic_light::TrafficLightClassifierNodelet",
+            name="car_traffic_light_classifier",
+            namespace="classification",
+            parameters=[
+                ParameterFile(
+                    param_file=LaunchConfiguration("car_traffic_light_classifier_param_path"),
+                    allow_substs=True,
+                ),
+                {
+                    "build_only": False,
+                },
+            ],
+            remappings=[
+                ("~/input/image", LaunchConfiguration("input/image")),
+                ("~/input/rois", LaunchConfiguration("output/rois")),
+                ("~/output/traffic_signals", "classified/car/traffic_signals"),
+            ],
+            extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
+        ),
+        ComposableNode(
+            package="autoware_traffic_light_classifier",
+            plugin="autoware::traffic_light::TrafficLightClassifierNodelet",
+            name="pedestrian_traffic_light_classifier",
+            namespace="classification",
+            parameters=[
+                ParameterFile(
+                    param_file=LaunchConfiguration(
+                        "pedestrian_traffic_light_classifier_param_path"
+                    ),
+                    allow_substs=True,
+                ),
+                {
+                    "build_only": False,
+                },
+            ],
+            remappings=[
+                ("~/input/image", LaunchConfiguration("input/image")),
+                ("~/input/rois", LaunchConfiguration("output/rois")),
+                ("~/output/traffic_signals", "classified/pedestrian/traffic_signals"),
+            ],
+            extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
+        ),
+        ComposableNode(
+            package="autoware_traffic_light_visualization",
+            plugin="autoware::traffic_light::TrafficLightRoiVisualizerNode",
+            name="traffic_light_roi_visualizer",
+            parameters=[create_parameter_dict("enable_fine_detection", "use_image_transport")],
+            remappings=[
+                ("~/input/image", LaunchConfiguration("input/image")),
+                ("~/input/rois", LaunchConfiguration("output/rois")),
+                ("~/input/rough/rois", "detection/rough/rois"),
+                ("~/input/traffic_signals", LaunchConfiguration("output/traffic_signals")),
+                ("~/output/image", "debug/rois"),
+                ("~/output/image/compressed", "debug/rois/compressed"),
+                ("~/output/image/compressedDepth", "debug/rois/compressedDepth"),
+                ("~/output/image/theora", "debug/rois/theora"),
+            ],
+            extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
+        ),
+    ]
+
+    traffic_light_node_loader = LoadComposableNodes(
+        composable_node_descriptions=traffic_light_nodes,
+        target_container=LaunchConfiguration("container"),
+    )
+
+    fine_detector_loader = LoadComposableNodes(
+        composable_node_descriptions=[
+            ComposableNode(
+                package="autoware_traffic_light_fine_detector",
+                plugin="autoware::traffic_light::TrafficLightFineDetectorNode",
+                name="traffic_light_fine_detector",
+                namespace="detection",
+                parameters=[
+                    ParameterFile(
+                        param_file=LaunchConfiguration("traffic_light_fine_detector_param_path"),
+                        allow_substs=True,
+                    ),
+                    {
+                        "build_only": False,
+                    },
+                ],
+                remappings=[
+                    ("~/input/image", LaunchConfiguration("input/image")),
+                    ("~/input/rois", "rough/rois"),
+                    ("~/expect/rois", "expect/rois"),
+                    ("~/output/rois", LaunchConfiguration("output/rois")),
+                ],
+                extra_arguments=[
+                    {"use_intra_process_comms": LaunchConfiguration("use_intra_process")}
+                ],
+            ),
+        ],
+        target_container=LaunchConfiguration("container"),
+        condition=IfCondition(LaunchConfiguration("enable_fine_detection")),
+    )
+
+    set_container_executable = SetLaunchConfiguration(
+        "container_executable",
+        "component_container",
+        condition=UnlessCondition(LaunchConfiguration("use_multithread")),
+    )
+
+    set_container_mt_executable = SetLaunchConfiguration(
+        "container_executable",
+        "component_container_mt",
+        condition=IfCondition(LaunchConfiguration("use_multithread")),
+    )
+
+    return LaunchDescription(
+        [
+            *launch_arguments,
+            set_container_executable,
+            set_container_mt_executable,
+            traffic_light_node_loader,
+            fine_detector_loader,
+        ]
+    )
