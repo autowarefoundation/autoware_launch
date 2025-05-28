4e75a50c037cb6be6189ce76dc553197ef379e2a
Takayuki Murooka <takayuki5168@gmail.com>
2025-05-29 03:02:39 +0900

    fix
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- autoware_launch/rviz/scripts/replace_displays_block.sh
+++ autoware_launch/rviz/scripts/replace_displays_block.sh
@@ -3,18 +3,18 @@
 FILE_REFERENCE="$1"
 FILE_TARGET="$2"
 
-# Displays ブロックを抽出（2スペースインデント想定）
+# extract Displays block
 extract_block() {
-  awk '
+    awk '
     $0 ~ /^  Displays:/ { in_block=1; print; next }
     in_block && $0 ~ /^   / { print; next }
     in_block { exit }
   ' "$1"
 }
 
-# Displays ブロックの行番号範囲を取得
+# get the row index of Displays
 get_block_range() {
-  awk '
+    awk '
     $0 ~ /^  Displays:/ { start=NR; next }
     start && $0 ~ /^   / { next }
     start { print start, NR-1; exit }
@@ -22,19 +22,19 @@ get_block_range() {
   ' "$1"
 }
 
-# ブロック範囲取得
-read start_line end_line < <(get_block_range "$FILE_TARGET")
+# get the scope of the block
+read -r start_line end_line < <(get_block_range "$FILE_TARGET")
 
-# 一時ファイル作成
+# create a temporary file
 tmp_file=$(mktemp)
 
-# 置換処理
+# replace the block
 {
-  head -n $((start_line - 1)) "$FILE_TARGET"
-  extract_block "$FILE_REFERENCE"
-  tail -n +"$((end_line + 1))" "$FILE_TARGET"
-} > "$tmp_file"
+    head -n $((start_line - 1)) "$FILE_TARGET"
+    extract_block "$FILE_REFERENCE"
+    tail -n +"$((end_line + 1))" "$FILE_TARGET"
+} >"$tmp_file"
 
-# 上書き or 出力
+# overwrite the file
 mv "$tmp_file" "$FILE_TARGET"
 echo "Updated 'Displays:' block of $FILE_TARGET"
